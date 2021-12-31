ac7109ccd98657e994a1a71a6a7cd5243d689f29
Adhritdas2009 <89207264+Adhritdas2009@users.noreply.github.com>
2021-12-31 18:10:34 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,4 @@
+player1=localStorage.getItem("User1");
+player2=localStorage.getItem("User2");
+document.getElementById("user1").innerHTML=player1;
+document.getElementById("user2").innerHTML=player2;
